900b0ac92bc1051b784dd8a50e1a665f0ed7eed7
Kyle Erdahl <kyle.m.erdahl@gmail.com>
2019-06-23 22:30:23 -0500

    Variable Declarations

--- main.js
+++ main.js
@@ -2,3 +2,7 @@
 exports.__esModule = true;
 var message = 'Hello World';
 console.log(message);
+var x = 10;
+var y = 20;
+var sum; // "let" can be reassigned and modified, but can't declare same variable more than once
+var title = 'Codevolution'; // "const" cannot be reassigned or modified, and can't declare the same variable more than once
--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 export { }
 let message = 'Hello World';
 console.log(message);
+
+let x = 10;
+let y = 20;
+
+let sum; // "let" can be reassigned and modified, but can't declare same variable more than once
+const title = 'Codevolution'; // "const" cannot be reassigned or modified, and can't declare the same variable more than once
